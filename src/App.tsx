6dd0927ec5e67ab4ad051327a601a62ab4e3fdbb
eduardo <eduardsanchoforeman@hotmail.com>
2022-04-27 13:04:29 +0200

    Ensuring we have a good response from the api

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
-import './App.css';
+import api_key from './api_key';
 
 function App() {
-  return <h1> Works </h1>;
+  const findMovies = async () => {
+    try {
+      const res = await fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${'the lord'}`
+      );
+      if (res.ok) {
+        const body = await res.json();
+        console.log('List of Movies', body);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  findMovies();
+
+  return <main className='movies-screen_container'></main>;
 }
 
 export default App;
