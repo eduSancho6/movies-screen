2108922ea30639825200c0c47b80f0a15d15a7b5
eduardo <eduardsanchoforeman@hotmail.com>
2022-04-27 20:37:47 +0200

    Refactorization of the component Movie

---
+++ src/components/Movie.tsx
@@ -0,0 +1,24 @@
+import MovieInterface from '../model';
+
+const Movie = ({
+  original_title,
+  poster_path,
+  backdrop_path,
+  id,
+}: MovieInterface) => {
+  return (
+    <article className='movie_card' key={id}>
+      <img
+        src={
+          poster_path
+            ? `https://image.tmdb.org/t/p/w300${poster_path}`
+            : `https://image.tmdb.org/t/p/w300${backdrop_path}`
+        }
+        alt={`${original_title}`}
+      ></img>
+      <h2> {original_title} </h2>
+    </article>
+  );
+};
+
+export default Movie;
