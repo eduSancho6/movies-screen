6321df44effd6922f6cb49322fc8c43f7b0ac567
eduardo <eduardsanchoforeman@hotmail.com>
2022-04-27 13:31:32 +0200

    We add the functional structure of the search engine

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import api_key from './api_key';
 
+interface MovieInterface {
+  original_title: string;
+  backdrop_path: string;
+  overview?: string;
+}
+
 function App() {
-  const findMovies = async () => {
+  const [search, setSearch] = useState<string>('the lord');
+  const [allMovies, setAllMovies] = useState<MovieInterface>();
+
+  const findMovies = async (e: React.FormEvent) => {
+    e.preventDefault();
     try {
       const res = await fetch(
-        `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${'the lord'}`
+        `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${search}`
       );
       if (res.ok) {
         const body = await res.json();
-        console.log('List of Movies', body);
+        console.log('List of Movies', body.results);
       }
     } catch (error) {
       console.error(error);
     }
   };
 
-  findMovies();
-
-  return <main className='movies-screen_container'></main>;
+  return (
+    <main className='movies-screen_container'>
+      <header>
+        <h2 className='header_title'> ¿Qúe vemos hoy? </h2>
+        <form className='search_form' onSubmit={findMovies}>
+          <input
+            type='text'
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className='search_input'
+          ></input>
+          <button type='submit' className='search_button'>
+            Search
+          </button>
+        </form>
+      </header>
+    </main>
+  );
 }
 
 export default App;
