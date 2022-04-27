7ccbd2c181f7994a787cfa4bc529022cf8ddc4b0
eduardo <eduardsanchoforeman@hotmail.com>
2022-04-27 21:24:28 +0200

    All the components created so far are refactored

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
 import React, { useState } from 'react';
-import api_key from './api_key';
-
-interface MovieInterface {
-  original_title: string;
-  backdrop_path?: string;
-  poster_path?: string;
-  overview?: string;
-}
+import API_KEY from './api_key';
+import ListOfMovies from './components/ListOfMovies';
+import SearchEngine from './components/SearchEngine';
+import MovieInterface from './model';
 
 function App() {
   const [search, setSearch] = useState<string>('the lord');
-  const [allMovies, setAllMovies] = useState<MovieInterface[]>();
+  const [allMovies, setAllMovies] = useState<MovieInterface[]>([
+    {
+      original_title: '',
+      id: 1,
+    },
+  ]);
 
-  const findMovies = async (e: React.FormEvent) => {
+  const searchMovie = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
       const res = await fetch(
-        `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${search}`
+        `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${search}`
       );
       if (res.ok) {
         const body = await res.json();
@@ -30,40 +31,12 @@ function App() {
 
   return (
     <main className='movies-screen_container'>
-      <section className='search-engine_container'>
-        <h2 className='header_title'>¿Qué vemos hoy?</h2>
-        <form className='search_form' onSubmit={findMovies}>
-          <input
-            type='text'
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-            className='search_input'
-          ></input>
-          <button type='submit' className='search_button'>
-            Buscar
-          </button>
-        </form>
-      </section>
-      <section className='movies-results_container'>
-        {allMovies
-          ? allMovies.map((movie, index) => {
-              console.log('movie', movie);
-              return (
-                <article className='movie_card' key={index}>
-                  <img
-                    src={
-                      movie.poster_path
-                        ? `https://image.tmdb.org/t/p/w300${movie.poster_path}`
-                        : `https://image.tmdb.org/t/p/w300${movie.backdrop_path}`
-                    }
-                    alt={`${movie.original_title}`}
-                  ></img>
-                  <h2> {movie.original_title} </h2>
-                </article>
-              );
-            })
-          : ''}
-      </section>
+      <SearchEngine
+        search={search}
+        setSearch={setSearch}
+        searchMovie={searchMovie}
+      />
+      <ListOfMovies allMovies={allMovies} />
     </main>
   );
 }
