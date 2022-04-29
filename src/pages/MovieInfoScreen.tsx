fc11881a7e20a9ac3c9e0b4b40db84e5b1991f55
eduardo <eduardsanchoforeman@hotmail.com>
2022-04-29 11:44:42 +0200

    Call to the api for a single Movie and its Page

--- src/pages/MovieInfoScreen.tsx
+++ src/pages/MovieInfoScreen.tsx
@@ -1,7 +1,73 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import API_KEY from '../api_key';
 
-const MovieInfoScreen = () => {
-  return <div>MovieInfoScreen</div>;
+const URL: string = 'https://api.themoviedb.org/3';
+
+interface MovieInterface {
+  original_title: string;
+  poster_path?: string;
+  overview?: string;
+  vote_average?: number;
+  release_date?: string;
+}
+
+const MovieInfoScreen: React.FC = () => {
+  const [movie, setMovie] = useState<MovieInterface>({
+    original_title: 'Waiting for the movie',
+    poster_path: '',
+    overview: 'Please, Wait until the movie is completely upload',
+    vote_average: 10,
+    release_date: '2022-02-22',
+  });
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const { id } = useParams();
+
+  const findMovieInfo = async (id: number) => {
+    try {
+      const res = await axios.get(`${URL}/movie/${id}`, {
+        params: {
+          api_key: API_KEY,
+        },
+        headers: {
+          'content-type': 'application/json',
+        },
+      });
+      setMovie(res.data);
+      setLoading(false);
+      console.log('A ver que pasa', res);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    findMovieInfo(Number(id));
+  }, []);
+
+  console.log('Movie', movie);
+
+  return (
+    <section className='movie-info-screen_container'>
+      {loading ? (
+        <h1>Loading</h1>
+      ) : (
+        <React.Fragment>
+          <button> Atrás </button>
+          <img
+            src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
+            alt={movie.original_title}
+          ></img>
+          <h1> {movie.original_title} </h1>
+          <p> {movie.overview} </p>
+          <span> {movie.vote_average} </span>
+          <p> {movie.release_date} </p>
+          {/* Meter géneros */}
+        </React.Fragment>
+      )}
+    </section>
+  );
 };
 
 export default MovieInfoScreen;
