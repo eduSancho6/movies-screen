2c1129d46f95ec4d228b1f4dadf5a9fe903d9bc1
eduardo <eduardsanchoforeman@hotmail.com>
2022-04-29 08:48:35 +0200

    Home Page ready

--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import MovieInterface from '../model';
+import axios from 'axios';
+import Header from '../components/ui/Header/Header';
+import SearchEngine from '../components/SearchEngine';
+import ListOfMovies from '../components/ListOfMovies';
+import API_KEY from '../api_key';
 
 const HomeScreen = () => {
-  return <div>HomeScreen</div>;
+  const [search, setSearch] = useState<string>('the lord');
+  const [allMovies, setAllMovies] = useState<MovieInterface[]>([
+    {
+      original_title: '',
+      id: 1,
+    },
+  ]);
+
+  const searchMovie = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const controller = new AbortController();
+
+    try {
+      const { data } = await axios.get(
+        `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${search}`
+      );
+      setAllMovies(data.results);
+      console.log('Info', data.results);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.error(error);
+      }
+    }
+    controller.abort();
+  };
+  return (
+    <React.Fragment>
+      <SearchEngine
+        search={search}
+        setSearch={setSearch}
+        searchMovie={searchMovie}
+      />
+      <ListOfMovies allMovies={allMovies} />
+    </React.Fragment>
+  );
 };
 
 export default HomeScreen;
