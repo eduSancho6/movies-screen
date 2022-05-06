138b0c36cae1f2119e53d4ecc46f4566ccbfb38c
eduardo <eduardsanchoforeman@hotmail.com>
2022-05-06 10:18:12 +0200

    Enrouting modified for fixing issues with the search

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,16 @@
-import HomeScreen from '../pages/HomeScreen';
+import SearchScreen from '../pages/SearchScreen';
 import MovieInfoScreen from '../components/MovieInfoScreen/MovieInfoScreen';
 import FavoriteScreen from '../pages/FavoriteScreen';
+import PopularMoviesScreen from '../pages/PopularMoviesScreen';
 
 const routes = [
-  { name: 'home', path: '/', component: <HomeScreen /> },
+  { name: 'home', path: '/', component: <PopularMoviesScreen /> },
+  {
+    name: 'searchResults',
+    path: '/search/:query',
+    component: <SearchScreen />,
+  },
+
   { name: 'movie', path: '/movie/:id', component: <MovieInfoScreen /> },
   { name: 'favorites', path: '/favs', component: <FavoriteScreen /> },
 ];
