3f091579c20d63adf7919c2291db09959d55212f
eduSancho6 <eduardsanchoforeman@hotmail.com>
2022-05-22 10:16:19 +0200

    first approach to an hero of the upcoming movies

--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { API_KEY } from '../api/api_key';
+import urlApi from '../api/urlApi';
 import Carousel from '../components/Carousel/Carousel';
 import SearchEngine from '../components/SearchEngine/SearchEngine';
 
@@ -15,11 +17,18 @@ import {
   fetchAsyncTopRatedMovies,
   selectAllTopRatedMovies,
 } from '../features/topRated/topRatedMoviesSlice';
+import MovieInterface from '../model';
+
+interface IListOIfMovies {
+  listOfMovies : MovieInterface[]
+}
 
 const HomeScreen = () => {
   const [indexPopular, setIndexPopular] = useState(0);
   const [indexRated, setIndexRated] = useState(0);
   const [indexPlaying, setIndexPlaying] = useState(0);
+
+  const [upcomingMovies, setUpcomingMovies] = useState()
 
   const dispatch = useDispatch();
 
@@ -33,11 +42,54 @@ const HomeScreen = () => {
   const topRatedMovies = useSelector(selectAllTopRatedMovies);
   const nowPlayingMovies = useSelector(selectNowPlayingMovies);
 
-  const linkPopulars = '/popular';
+  console.log('Popular movies', popularMovies)
+
+  const linkPopulars = '/popular'; 
   const linkRated = '/rated';
+
+
+
+  const getUpcomingMovies = async() => {
+    try {
+      const {data} = await urlApi.get(`/movie/upcoming?api_key=${API_KEY}&page=1`)
+      console.log('Upcoming Movies',data.results)
+      setUpcomingMovies(data.results)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  useEffect(()=> {
+    getUpcomingMovies()
+  },[])
+
+
+
+  const Hero= ({listOfMovies}:IListOIfMovies) => {
+    const [counter, setCounter] = useState(0)
+    console.log('popularMovies', popularMovies)
+    return (
+      <section className="hero-movies_container">
+
+        <article className="upcoming-movie_container" >
+      {
+        listOfMovies[2].backdrop_path &&  <img alt="movie" src={`https://image.tmdb.org/t/p/w1280${listOfMovies[2].backdrop_path}`} ></img>
+      }
+
+        </article>
+        
+      </section>
+    )
+  }
+
+
 
   return (
     <React.Fragment>
+
+{
+  upcomingMovies && <Hero listOfMovies={upcomingMovies}/>
+}
+
       <SearchEngine />
 
       <Carousel
