ac52b3f5f59658e3dfe345d0ffdb769c4931dd92
eduSancho6 <eduardsanchoforeman@hotmail.com>
2022-05-24 13:10:06 +0200

    Basic structure off the hero

--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -4,6 +4,7 @@ import { API_KEY } from '../api/api_key';
 import urlApi from '../api/urlApi';
 import Carousel from '../components/Carousel/Carousel';
 import SearchEngine from '../components/SearchEngine/SearchEngine';
+import { AiOutlineArrowRight, AiOutlineArrowLeft } from 'react-icons/ai'
 
 import {
   fetchAsyncPopularMovies,
@@ -64,15 +65,27 @@ const HomeScreen = () => {
 
 
 
-  const Hero= ({listOfMovies}:IListOIfMovies) => {
+  const Hero = ({listOfMovies}:IListOIfMovies) => {
     const [counter, setCounter] = useState(0)
-    console.log('popularMovies', popularMovies)
+
     return (
       <section className="hero-movies_container">
+        <div className="title-hero_container">
+          <h2 className="title-hero">Próximamente en los <span className="underline-red">mejores</span>  cines...</h2>
+        </div>
 
         <article className="upcoming-movie_container" >
+          <div className="buttons-hero_container">
+              <button disabled={} onClick={() => {
+                setCounter(counter - 1)
+              }} className="btn-arrow arrow-left"> <AiOutlineArrowLeft className="icon-arrow"/> </button>
+              <button onClick={() => {
+                setCounter(counter + 1)
+              }} className="btn-arrow arrow-right"> <AiOutlineArrowRight className="icon-arrow" /> </button>
+
+          </div>
       {
-        listOfMovies[2].backdrop_path &&  <img alt="movie" src={`https://image.tmdb.org/t/p/w1280${listOfMovies[2].backdrop_path}`} ></img>
+        listOfMovies[2].backdrop_path &&  <img alt="movie" src={`https://image.tmdb.org/t/p/w1280${listOfMovies[counter].backdrop_path}`} ></img>
       }
 
         </article>
