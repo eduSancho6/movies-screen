7ab5c123d978e6c0d015c3a7510667d60afdeb32
eduSancho6 <eduardsanchoforeman@hotmail.com>
2022-05-25 15:03:16 +0200

    hero improvements

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai'
+import { GiCircle, GiPlainCircle } from 'react-icons/gi'
+import { BsArrowRightCircleFill, BsArrowLeftCircleFill } from 'react-icons/bs'
 import MovieInterface from '../../model'
 import './hero.css'
 
@@ -8,7 +10,7 @@ type IListOfMovies = {
 }
 
 const Hero = ({listOfMovies}:IListOfMovies) => {
-    const [counter, setCounter] = useState(0)
+    const [counter, setCounter] = useState(0);
   
   /*   setInterval(() => {
       if(counter !== 19) {
@@ -32,11 +34,39 @@ const Hero = ({listOfMovies}:IListOfMovies) => {
             <div className="buttons-hero_container">
                 <button disabled={counter === 0} onClick={() => {
                   setCounter(counter - 1)
-                }} className="btn-arrow arrow-left"> <AiOutlineArrowLeft className="icon-arrow"/> </button>
+                }} className="btn-arrow arrow-left"> <BsArrowLeftCircleFill className="icon-arrow"/> </button>
                 <button disabled={ counter === 19} onClick={() => {
                   setCounter(counter + 1)
-                }} className="btn-arrow arrow-right"> <AiOutlineArrowRight className="icon-arrow" /> </button>
+                }} className="btn-arrow arrow-right"> <BsArrowRightCircleFill className={`icon-arrow `} /> </button>
   
+            </div>
+            <div className="litle-buttons-hero_container">
+              <button className="mini-arrow mini-arrow-left" onClick={() => {
+                if( counter === 0) {
+                  setCounter(19)
+                } else {
+                  setCounter(counter - 1)
+                }
+                }}
+                ><AiOutlineArrowLeft /></button>
+
+                {
+                  listOfMovies.map((movie, key) => {
+                    return(
+                      
+                      <button key={key} className="little-button" onClick={() => setCounter(key)} type='submit'><GiCircle className={`little-icon ${counter ===  key ? "red" : ""}`} /> </button>
+                    )
+                  })
+                }
+              <button className="mini-arrow mini-arrow-right" onClick={() =>{
+                                if( counter === 19 ) {
+                                  setCounter(0)
+                                } else {
+                                  setCounter(counter +1)
+                                }
+              }}><AiOutlineArrowRight /></button>
+ 
+
             </div>
         {
           listOfMovies[2].backdrop_path &&  <img alt="movie" src={`https://image.tmdb.org/t/p/w1280${listOfMovies[counter].backdrop_path}`} ></img>
